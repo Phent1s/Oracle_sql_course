2c190655f46025847f1143fc6f1af7b6b03c17c1
phent1s <phent1soff@gmail.com>
2024-07-14 22:43:40 +0300

    SELECT

---
+++ 2_1 SELECT.sql
@@ -0,0 +1,11 @@
+select * from employees;
+
+select * from departments;
+
+select * from regions;
+
+select* from countries;
+
+select country_id, country_name, region_id from countries;
+
+select first_name, salary, last_name from employees;
