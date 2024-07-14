15a7be13dee3e40e553c089030e3b4e66a4b2507
phent1s <phent1soff@gmail.com>
2024-07-14 22:43:57 +0300

    DISTINCT

---
+++ 2_2 DISTINCT.sql
@@ -0,0 +1,9 @@
+select job_id from job_history;
+
+select distinct job_id from job_history;
+
+select distinct job_id, department_id from job_history;
+
+select distinct employee_id, start_date, job_id from job_history;
+
+select distinct commission_pct from employees;
