527c4002f5b9ef1dff2448af4dd2e0e458543a3e
phent1s <phent1soff@gmail.com>
2024-07-14 22:44:51 +0300

    DUAL and q

---
+++ 2_6 DUAL and q.sql
@@ -0,0 +1,15 @@
+select 'abc' || 'xyz' || (90*30) from employees;
+
+select * from dual;
+
+desc dual;
+
+select 'abc' || 'xyz' || (90*30) as result from dual;
+
+select 18 * 365 * 24*60*60 "moy cozrast v seeekundah" from dual;
+
+select 7*8 from dual;
+
+select 'it''s my life' from dual;
+
+select q'<it's my life>' as song from dual;
