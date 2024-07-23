a5eb45794a249593376ee1bd2d43d78894d72daa
phent1s <phent1soff@gmail.com>
2024-07-23 23:55:51 +0300

    4_8 TRIM.sql

---
+++ 4_8 TRIM.sql
@@ -0,0 +1,9 @@
+select trim(trailing 'q' from 'qqqqqqZaurqqqqqq') from dual;
+
+select trim(leading 'q' from 'qqqqqqZaurqqqqqq') from dual;
+
+select trim('q' from 'qqqqqqZaurqqqqqq') from dual;
+
+select trim(' ' from  '            Zaurqqqqqq      Fadsdawdsadwd      ') from dual;
+
+select trim('      Zaur  Trigulow      ') from dual;
