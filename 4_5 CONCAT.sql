4c4fc4ecbe06919aba1bf8c28cb21fce0ec6b514
phent1s <phent1soff@gmail.com>
2024-07-23 23:55:00 +0300

    4_5 CONCAT.sql

---
+++ 4_5 CONCAT.sql
@@ -0,0 +1,8 @@
+select CONCAT('privet', ' drug') from dual;
+select CONCAT(55 ,' drug') from dual;
+select concat(salary*2, hire_date) from employees;
+select sysdate from dual;
+
+select concat('Today is ', sysdate) from dual;
+
+select concat('Privet', concat('moy', 'drug')) from dual;
